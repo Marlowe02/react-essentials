4dc98ccc2a6afa5a28b3d11f1848de92bc413b6a
Marlowe02 <154118075+Marlowe02@users.noreply.github.com>
2024-02-15 21:16:29 +0800

    Initial commit

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,9 @@
+export default function Tabs({ children, buttons, ButtonsContainer = "menu" }) {
+  // const ButtonsContainer = ButtonsContainer;
+  return (
+    <>
+      <ButtonsContainer>{buttons}</ButtonsContainer>
+      {children}
+    </>
+  );
+}
